69c01565fdea1e19b5f5ffe682bb7a0c0da76329
przemuh <przemuh@gmail.com>
2022-03-21 20:46:00 +0100

    Loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+import "./Loader.css";
+
+export const Loader = () => (
+  <div className="lds-ring">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
